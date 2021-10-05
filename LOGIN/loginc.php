b45abc83e9f33207476c4fad7772ed8bde1e1ece
TasnimJarin <tasnim.jarin.2000@gmail.com>
2021-10-05 23:01:58 +0600

    Login system added

--- LOGIN/loginc.php
+++ LOGIN/loginc.php
@@ -1,34 +1,37 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Name Field </title>
-</head>
-<body>	
-	<form method="post">
-		<fieldset>
-			<legend>Form</legend>
-            Name:<input type="text" name="myname" value="<?php 
+<?php 
 
 if(isset($_REQUEST['submit']))
 {
-	$name = $_REQUEST['myname'];
-	if($name == ""){
-		echo "null value...";
-	}else{
-		echo $name;
+	$name = $_REQUEST['name'];
+	$password = $_REQUEST['password'];
+
+	if($name !="")
+	{
+		if($password !="")
+		{
+             if($name==$password)
+			 {
+				header('location: Home.html');
+			 }
+			 else
+			 {
+				 echo "Invalid username/password";
+			 }
+		}
+		else
+		{
+			echo "Invalid password";
+		}
+		
+	}
+	else
+	{
+		echo "Invalid username";
 	}	
 }
  
 
-?>"><br/><hr>
-
-            password:<input type="password"name="" value="">
-            <input type="submit" name="submit" value="Submit">
-         
-		</fieldset>
-	</form>
-</body>
-</html>
+?>
 
 
 
--- LOGIN/regc.php
+++ LOGIN/regc.php
@@ -1,53 +1,41 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Name Field </title>
-</head>
-<body>	
-	<form method="post">
-		<fieldset>
-			<legend>Registration</legend>
-            Name:<input type="text" name="myname" value="<?php 
+<?php 
 
 if(isset($_REQUEST['submit']))
 {
-	$name = $_REQUEST['myname'];
-	if($name == ""){
-		echo "null value...";
-	}else{
-		echo $name;
+	$name = $_REQUEST['name'];
+    $email=$_REQUEST['email'];
+    $passwor=$_REQUEST['password'];
+ 
+	if($name !="")
+    {
+		if($email !="")
+        {
+            if($passwor !="")
+            {
+                if($name==$passwor )
+                {
+                    header('location: Login.html');
+                }
+                else
+                {
+                    echo "Invalid...";
+                }
+            }
+            else
+            {
+                 echo "Invalid password..";
+            }
+        }
+        else
+        {
+         echo "Invalid email..";
+        }
+	}
+    else
+    {
+		echo "Invalid name";
 	}	
 }
 
 
-?>"><br/><hr>
-
-        Email:<input type="email" name="myemail" value="<?php
-    if(isset($_REQUEST["submit"]))
-    {
-
-        $email=$_REQUEST["myemail"];
-           if($email=="")
-           {
-               echo "null value...";
-           }
-           else
-           {
-               echo $email;
-           }
-    }
-   
-
-?>"><br/><hr>
-
-            password:<input type="password"name="" value="">
-            <input type="submit" name="submit" value="Submit">
-         
-		</fieldset>
-	</form>
-</body>
-</html>
-
-
-
-
+?>

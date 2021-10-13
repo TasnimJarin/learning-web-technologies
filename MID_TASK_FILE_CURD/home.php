9a5352490c2b6df00f3845baea5876de96afb6d3
TasnimJarin <tasnim.jarin.2000@gmail.com>
2021-10-13 23:53:59 +0600

    Task file curd

--- MID_TASK_FILE_CURD/home.php
+++ MID_TASK_FILE_CURD/home.php
@@ -6,19 +6,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
 	<title>Home Page</title>
 </head>
 <body>
-	<h1>Welcome Home!</h1>
+	<center>
+	<u><h1>Welcome Home!</h1></u>
 	<a href="create.php"> Create New User</a> |
 	<a href="userlist.php"> User List</a> |
-	<a href="logout.php"> logout</a>
+	<a href="logout.php"> Logout</a>
+	</center>
 </body>
 </html>
 
 <?php
-	}else{
+	}
+	else
+	{
 		header('location: login.html');
 	}
 ?>

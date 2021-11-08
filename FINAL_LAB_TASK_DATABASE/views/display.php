df77a366cafafa97ab284ef98231f3f5bafe6cc6
TasnimJarin <tasnim.jarin.2000@gmail.com>
2021-11-08 16:41:11 +0600

    task

--- FINAL_LAB_TASK_DATABASE/views/display.php
+++ FINAL_LAB_TASK_DATABASE/views/display.php
@@ -1,18 +1,45 @@
+<?php
+     
+     require_once('../model/usersModel.php');
 
+
+?>
+
+
+
+<!DOCTYPE html>
 <html>
 <head>
 	<title>DISPLAY</title>
 </head>
-
 <body>
-	
-	<form method="post">
-		<fieldset>
-			<legend>DISPLAY</legend>
-			<table>
-				
-			</table>
-		</fieldset>
-	</form>
+
+	<br>
+	<table border="2" align="center">
+		<tr>
+			<th>NAME</th>
+			<th>PROFIT</th>
+			<th colspan="2"></th>
+			
+		</tr>
+		<?php
+            
+            $result =getAllProduct();
+		      while($user= mysqli_fetch_assoc($result))
+            {  ?>
+                
+             <tr>
+				<td><?=$user['name']?></td>
+				<td><?=$user['buying']?></td>
+				<td>
+				<a href="edit.php?edit=<?=$user['id'] ?>"> EDIT</a> |
+				<a href="delete.php?delete=<?=$user['id']?>"> DELETE</a>
+				</td>
+			 </tr>
+
+         <?php
+            }
+		?>
+	</table>
 </body>
 </html>
---
+++ FINAL_LAB_TASK_DATABASE/views/edit.php
@@ -0,0 +1,42 @@
+
+
+<html>
+<head>
+	<title>EDIT</title>
+</head>
+
+<body>
+	
+	<form method="post">
+		<fieldset>
+			<legend>EDIT PRODUCT</legend>
+			<table>
+				<tr>
+					<td>Name:</td>
+					<td><input type="text" name="name" value=""></td>
+				</tr>
+				<tr>
+					<td>Buying Price:</td>
+					<td><input type="number" name="buying" value=""></td>
+				</tr>
+				<tr>
+					<td>Selling Price:</td>
+					<td><input type="number" name="selling" value=""></td>
+				</tr>
+                <tr>
+					<td><input type="checkbox"name="dis" value="dis">Display</td>
+					<td></td>
+				</tr>
+                <tr>
+					<td colspan="2"><hr/></td>
+					
+				</tr>
+				<tr>
+					<td><input type="submit" name="submit" value="SAVE"></td>
+					<td></td>
+				</tr>
+			</table>
+		</fieldset>
+	</form>
+</body>
+</html>

e0ba44712877374700c8f9766eeb637446926113
TasnimJarin <tasnim.jarin.2000@gmail.com>
2021-11-24 15:57:49 +0600

    comit 2

--- FINAL_LAB_PERFORMANCE_2/script.js
+++ FINAL_LAB_PERFORMANCE_2/script.js
@@ -1,4 +1,4 @@
-function ajax(){
+function ajax(num1, num2, operator, key){
 
 	let name = document.getElementById('n').value;
 	let xhttp= new XMLHttpRequest();
@@ -14,4 +14,24 @@ function ajax(){
 			document.getElementById('s').innerHTML = this.responseText;		
 		}
 	}
+}
+function getResult(num1, num2, operator, key) {
+    let xhttp = new XMLHttpRequest();
+  
+    xhttp.open("POST", "jsGotNoOperators.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("num1=" + num1 + "&&num2=" + num2 + "&&operator=" + operator);
+  
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        //document.getElementById('h1').innerHTML = this.responseText;
+        //console.log(this.responseText);
+        //return this.responseText;
+        resultPrevElement.innerHTML = this.responseText;
+        resultElement.innerHTML = 0;
+        operatorElement.innerHTML = key;
+        hideElementsForEqual(key);
+      }
+    }
+
 }
